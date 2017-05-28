cbd740bd99964f3c45fc32e554e0fb87cde3c8ac
Unknown <gonzalozasreg@gmail.com>
2017-05-28 16:36:34 +0200

    Drag blocks from Block Menu implemented

--- build/Main.js
+++ build/Main.js
@@ -1,1 +1,33 @@
 "use strict";
+$(".BloqueEnMenu").draggable({
+    helper: 'clone',
+    cursor: 'move',
+    tolerance: 'fit',
+    revert: true,
+    start: function (evento, ui) {
+        var bloqueQueManejo = ui.helper;
+        $('.menuBloques').css('overflow-x', 'visible');
+    },
+    stop: function (evento, ui) {
+        var bloqueQueManejo = ui.helper;
+        $('.menuBloques').css('overflow-x', 'hidden');
+    }
+});
+$(".areaBloques").droppable({
+    accept: function (draggable) {
+        if (draggable.hasClass("BloqueEnMenu") || draggable.hasClass("BloqueArrastrable")) {
+            return true;
+        }
+    },
+    drop: function (evento, ui) {
+        if ($(ui.helper).hasClass('BloqueEnMenu')) {
+            var dragEl = ui.helper.clone();
+            var stopPosition = ui.helper.position();
+            var categoria = $(ui.helper).attr('class').split(' ')[0];
+            var tipo = $(ui.draggable).attr("id");
+            ui.helper.remove();
+            var bloqueClon = new BloqueArrastrable(categoria, tipo, $(evento.target).get(0));
+            $(bloqueClon.miDiv).css({ top: stopPosition.top, left: stopPosition.left });
+        }
+    }
+});
--- ts/Main.ts
+++ ts/Main.ts
@@ -1,1 +1,45 @@
+$(".BloqueEnMenu").draggable({
 
+    helper: 'clone',
+    cursor: 'move',
+    tolerance: 'fit',
+    revert: true,
+
+    start: function(evento: Event, ui: JQuery){
+
+        let bloqueQueManejo = ui.helper;
+        $('.menuBloques').css('overflow-x', 'visible');
+    },
+    stop: function(evento: Event, ui: JQuery){
+
+        let bloqueQueManejo = ui.helper;
+        $('.menuBloques').css('overflow-x', 'hidden');
+    }
+});
+
+
+$(".areaBloques").droppable({
+
+    accept: function(draggable: JQuery) {
+        if(draggable.hasClass("BloqueEnMenu") || draggable.hasClass("BloqueArrastrable")){
+            return true;
+        }
+    },
+
+    drop: function(evento: Event, ui: JQuery) {
+
+        if($(ui.helper).hasClass('BloqueEnMenu')){
+
+            var dragEl = ui.helper.clone();
+
+            var stopPosition = ui.helper.position();
+            var categoria: string = $(ui.helper).attr('class').split(' ')[0];
+            var tipo: string = $(ui.draggable).attr("id");
+
+            ui.helper.remove();
+
+            let bloqueClon: BloqueArrastrable = new BloqueArrastrable(categoria, tipo, $(evento.target).get(0));
+            $(bloqueClon.miDiv).css({top: stopPosition.top, left: stopPosition.left})
+        }
+    }
+});
