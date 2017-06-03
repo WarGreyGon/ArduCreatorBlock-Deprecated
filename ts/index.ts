44843327eb7d3b105e82d4348ed0790167146e46
Unknown <iria.carro.llorente@gmail.com>
2017-06-04 00:20:28 +0200

    Register box improvements

--- ts/index.ts
+++ ts/index.ts
@@ -15,31 +15,56 @@ $(function(){
     });
 });
 
-function createElement(){
+function createElement() :void{
     $("#formContent").hide();
-    var div = document.createElement("div");
-    div.className = "form-group";
-    div.id = "divRegisterInput";
-    var label = document.createElement("label");
-    label.className = "Label";
-    label.innerHTML = "CORREO ELECTRÓNICO";
-    div.appendChild(label);
-    var input = document.createElement("input");
-    input.className = "Input";
-    input.type = "text";
-    input.name = "email";
-    input.placeholder = "Correo Electrónico";
-    div.appendChild(input);
-    $("#formContent").prepend(div);
+    $("#formBox").css("margin-top", "0");
+    var divEmail = createDiv("divRegisterInput");
+    var labelEmail = createLabel("CORREO ELECTRÓNICO");
+    divEmail.appendChild(labelEmail);
+    var inputEmail = createInput("email", "Correo Electrónico");
+    divEmail.appendChild(inputEmail);
+    $("#formContent").prepend(divEmail);
+
+    var divRepeatPass = createDiv("divRepeatPass");
+    var labelPassword = createLabel("REPITA LA CONTRASEÑA");
+    divRepeatPass.appendChild(labelPassword);
+    var inputPassword = createInput("passwordRepeat", "Repita la Contraseña");
+    divRepeatPass.appendChild(inputPassword);
+    insert(divRepeatPass, document.getElementById("password"));
+
     $(".Signin").text("Iniciar Sesión");
     $(".Signup").text("Enviar");
     $("#formContent").slideToggle();
 }
-
-function removeElement(){
+function removeElement() :void{
     $("#formContent").hide();
+    $("#formBox").css("margin-top", "6%");
     $("#divRegisterInput").remove();
+    $("#divRepeatPass").remove();
     $(".Signup").text("Registrarse");
     $(".Signin").text("Enviar");
     $("#formContent").slideToggle();
+}
+function createDiv(id: string) : HTMLElement{
+    var div = document.createElement("div");
+    div.className = "form-group";
+    div.id = id;
+    return div;
+}
+function createLabel(name: string) :HTMLElement{
+    var label = document.createElement("label");
+    label.className = "Label";
+    label.innerHTML = name;
+    return label;
+}
+function createInput(name: string, placeholder: string) :HTMLElement{
+    var input = document.createElement("input");
+    input.className = "Input";
+    input.type = "text";
+    input.name = name;
+    input.placeholder = placeholder;
+    return input;
+}
+function insert(newNode :HTMLElement, referenceNode:HTMLElement) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
